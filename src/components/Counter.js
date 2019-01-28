93abd8e72dce83a33a743edbc2fa23580e11b722
umbur <work1415@gmail.com>
2019-01-28 16:26:29 -0700

    Stretch goals completed: increment if odd and increment async

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,13 +6,16 @@ class Counter extends Component {
     incrementIfOdd = () => {
         // Stretch Problem: Implement an increment function that
         // only increments if the counter value is odd
+        if (this.props.count % 2 !== 0) {
+            this.props.increment(this.state)
+        }
     };
 
     incrementAsync = () => {
         // Stretch Problem: Implement an increment function that
         // increments after waiting for one second
+        setTimeout(this.props.increment, 1000);
     };
-
     render() {
         // Fill in the two button onClick methods
         // Upon clicking these buttons, the count
@@ -28,12 +31,12 @@ class Counter extends Component {
                 </button>
                  {/* Uncomment these button tags if you got
                 around to implementing the extra credit functions */}
-                {/* <button onClick={this.incrementIfOdd}>
+                 <button onClick={this.incrementIfOdd}>
                     Increment if odd
                 </button>
                 <button onClick={this.incrementAsync}>
                     Increment async
-                </button>  */}
+                </button>  
             </p>
         );
     }
