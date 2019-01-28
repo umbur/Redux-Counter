71ffea37d85be97e5925f1bacc5645525f53bf4b
umbur <work1415@gmail.com>
2019-01-28 15:38:44 -0700

    Initial commit, plus installed yarn

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -9,6 +9,7 @@ const initialState = {
 // of our redux store, along with an action created
 // by our action creator. What does the reducer
 // need to do with the count in each case?
+// initial commit
 export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
